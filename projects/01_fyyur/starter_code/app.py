3cc774b9ec4c2f16c963c6d9e4175a1e56dcf4b4
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2022-07-27 16:33:31 +0100

    Change launch to default to port 3000

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -12,6 +12,8 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+import os
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -511,11 +513,4 @@ if not app.debug:
 
 # Default port:
 if __name__ == '__main__':
-    app.run()
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+    app.run(port=os.environ.get('PORT', 3000), debug=True)
